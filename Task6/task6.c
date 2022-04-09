d4cbed4884765602726a7e5e19e9e5fad84ec35d
mariamabdelati <mariam.tamer241@gmail.com>
2022-04-09 19:28:04 +0200

    completed task6 with comments

--- Task6/task6.c
+++ Task6/task6.c
@@ -16,3 +16,36 @@ the output would be:
 You should use an appropriate data type to store 32 bits. Also, it will be a good idea to use
 unsigned data types as well.
 */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+// define a constant page size of 4-KB as specified
+#define PAGE_SIZE 4096
+
+int main(int argc, char *argv[]) {
+
+    // declare unsigned integers since the addresses can never be negative (32-bit)
+    unsigned int address;
+    unsigned int page_number;
+    unsigned int offset;
+
+    // if less than 1 parameter are passed, the virtual address is missing 
+    if (argc < 2){
+        printf("At least 1 parameter is needed\n");
+        exit(1);
+    }
+
+    // convert the string passed on command line to integer and  save it to address
+    address = atoi(argv[1]);
+
+    // the page number is equivalent to the address divided by the page size of 4-KB
+    page_number = address / PAGE_SIZE;
+
+    // the offset is equivalent to remainder of dividing the address by the page size of 4-KB
+    offset = address % PAGE_SIZE;
+
+    printf("The address %u contains: \n  page number = %u\n  offset = %u\n", address, page_number, offset);
+
+    return 0;
+}
